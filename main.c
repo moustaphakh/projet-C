2a241e110d60fef989b4bd99f13813ba52363a74
moustaphakh <152089001+moustaphakh@users.noreply.github.com>
2024-03-29 10:02:53 +0100

    configuration commit

---
+++ main.c
@@ -0,0 +1,5 @@
+//
+// Created by Mousti on 3/29/2024.
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by Mousti on 3/29/2024.
+//
+
+#ifndef PROJET_C_MAIN_H
+#define PROJET_C_MAIN_H
+
+#endif //PROJET_C_MAIN_H
