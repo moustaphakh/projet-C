eeb9289cd9e67ca13894eeda8fdde755cd12865c
Savratis56 <151128419+Savratis56@users.noreply.github.com>
2024-04-21 17:56:08 +0200

    Update column.h

--- column.h
+++ column.h
@@ -1,1 +1,37 @@
+#include<stdio.h>
+#ifndef PROJET_C_COLUMN_H
+#define PROJET_C_COLUMN_H
+typedef struct {
+    char* title;
+    int physical_size;
+    int logical_size;
+    int* data;
+}COLUMN;
+
+COLUMN *create_column(char* title);
+int insert_value(COLUMN* col, int value);
+void delete_column(COLUMN **col);
+void print_col(COLUMN* col);
+int countOccurrences(int array[], int size, int x);
+int getValueAtIndex(int array[], int size, int index);
+int countValuesGreaterThanX(int array[], int size, int x);
+int countValuesLessThanX(int array[], int size, int x);
+int countValuesEqualToX(int array[], int size, int x);
+
+
+
+
+#endif //PROJET_C_COLUMN_H
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
 
