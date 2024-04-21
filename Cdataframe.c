263357ff68c02c9cbb3ec9e5bb8fc53f5d2be236
moustaphakh <152089001+moustaphakh@users.noreply.github.com>
2024-04-21 23:18:37 +0200

    Update Cdataframe.c

--- Cdataframe.c
+++ Cdataframe.c
@@ -1,1 +1,21 @@
+#include "cdataframe.h"
+#include <stdio.h>
+
+
+CDataframe *create_cdataframe(int initial_capacity) {
+    CDataframe *df = (CDataframe *)malloc(sizeof(CDataframe));
+    if (df == NULL) {
+        return NULL;
+    }
+
+    df->columns = (COLUMN **)malloc(initial_capacity * sizeof(COLUMN *));
+    if (df->columns == NULL) {
+        free(df);
+        return NULL;
+    }
+
+    df->num_columns = 0;
+    df->capacity = initial_capacity;
+    return df;
+}
 
