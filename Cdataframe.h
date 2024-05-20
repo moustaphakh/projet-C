68b4f8685e1b5882f713b9555310d3d0f829250b
moustaphakh <152089001+moustaphakh@users.noreply.github.com>
2024-05-20 23:53:58 +0200

    Update Cdataframe.h

--- Cdataframe.h
+++ Cdataframe.h
@@ -5,33 +5,27 @@
 
 typedef struct {
     COLUMN **columns;
-    int num_columns;  
-    int capacity;     
+    int num_columns;
+    int capacity;
 } CDataframe;
-
 
 CDataframe *create_cdataframe(int initial_capacity);
 void delete_cdataframe(CDataframe *df);
 
-
 void fill_cdataframe_with_input(CDataframe *df);
-void hard_fill_cdataframe(CDataframe *df); 
-
+void hard_fill_cdataframe(CDataframe *df, int num_columns, int num_rows);
 
 void display_cdataframe(const CDataframe *df);
 void display_partial_cdataframe(const CDataframe *df, int start_row, int end_row, int start_col, int end_col);
 
-
-void add_row_to_cdataframe(CDataframe *df, int *row_values); 
+void add_row_to_cdataframe(CDataframe *df, int *row_values);
 void delete_row_from_cdataframe(CDataframe *df, int row_index);
-void add_column_to_cdataframe(CDataframe *df, COLUMN *col); 
+void add_column_to_cdataframe(CDataframe *df, COLUMN *col);
 void delete_column_from_cdataframe(CDataframe *df, int col_index);
 void rename_column_in_cdataframe(CDataframe *df, int col_index, char *new_title);
 int search_value_in_cdataframe(const CDataframe *df, int value);
 int replace_value_in_cdataframe(CDataframe *df, int row, int col, int new_value);
 
-
 void display_cdataframe_stats(const CDataframe *df);
 
 #endif // CDATAFRAME_H
-
